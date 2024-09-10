29eccd9c88202f57cc72e52b922b6a02e4b2954c
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2024-09-10 00:00:04 -0300

    Se elimina lógica db en form

---
+++ Business/Managers/ArticuloManager.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Managers
+{
+    public class ArticuloManager
+    {
+        private DBManager dbManager;
+
+        public ArticuloManager()
+        {
+            dbManager = new DBManager();
+        }
+
+        public string ObtenerNombreArticuloPorId(int id)
+        {
+            try
+            {
+                dbManager.OpenConnection();
+
+                DataTable tabla = dbManager.ExecuteQuery($"Select Nombre From ARTICULOS where Id = {id}");
+
+                if (tabla.Rows.Count > 0)
+                {
+                    return tabla.Rows[0]["Nombre"].ToString();
+                }
+                else
+                {
+                    return "Artículo no encontrado";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar el error de manera más amigable para la capa UI
+                throw new Exception("Error al obtener el artículo.", ex);
+            }
+            finally
+            {
+                dbManager.CloseConnection();
+            }
+        }
+    }
+}
