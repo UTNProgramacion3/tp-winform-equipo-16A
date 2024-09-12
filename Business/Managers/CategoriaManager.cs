fb085f31868958968afeb2d9867f3209672eb4cf
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2024-09-11 22:45:26 -0300

    Add: categoria + manager

---
+++ Business/Managers/CategoriaManager.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Managers
+{
+    public class CategoriaManager : ICrudRepository<Categoria>
+    {
+        private DBManager dbManager;
+
+        public CategoriaManager()
+        {
+            dbManager = new DBManager();
+        }
+
+        public void Crear(Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Eliminar(Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria ObtenerPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Categoria> ObtenerTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
